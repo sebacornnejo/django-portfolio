13fafd9627cef16bfcd4c482fc011d8ffb5c95fa
sebacornnejo <scornejoguz@gmail.com>
2023-12-14 23:55:33 -0300

    3rd deploy to render

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -92,7 +92,11 @@ WSGI_APPLICATION = "django_portfolio.wsgi.application"
 
 DATABASES = {
     "default": {
-        dj_database_url.config()
+        dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
         # "ENGINE": "django.db.backends.sqlite3",
         # "NAME": BASE_DIR / "db.sqlite3",
         
