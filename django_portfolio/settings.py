08bf17c2f87432057c3c25d398c597562db428d4
sebacornnejo <scornejoguz@gmail.com>
2023-12-16 16:20:51 -0300

    problem with sql images 17

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -176,7 +176,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 #     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/static/media/"
 # MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
