25e3f95ec8eb56fed039620203c3010504ef994d
sebacornnejo <scornejoguz@gmail.com>
2023-12-15 23:42:03 -0300

    Change MEDIA_ROOT

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -171,7 +171,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 #     # and creating unique names for each version so they can safely be cached forever.
 #     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_ROOT = BASE_DIR / "media"
+# MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = "/public/"
 
