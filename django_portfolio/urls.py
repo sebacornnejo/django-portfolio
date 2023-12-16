92b5173187163126c02bba04ec70dcc091c2b3ad
sebacornnejo <scornejoguz@gmail.com>
2023-12-15 23:51:14 -0300

    Change urls.py

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -35,4 +35,4 @@ urlpatterns = [
     path('thankyou/', views.thankyou,name='thankyou'),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
