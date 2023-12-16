b1114c8331d06e88568dd5ac7007b9459c34d252
sebacornnejo <scornejoguz@gmail.com>
2023-12-16 00:43:44 -0300

    problem with sql images

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -174,7 +174,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = "/public/"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
