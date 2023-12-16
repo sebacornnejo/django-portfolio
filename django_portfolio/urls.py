440b04ab45d81194bb60647ea002d6755cac0a84
sebacornnejo <scornejoguz@gmail.com>
2023-12-16 01:48:56 -0300

    problem with sql images 07

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -35,4 +35,5 @@ urlpatterns = [
     path('thankyou/', views.thankyou,name='thankyou'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
