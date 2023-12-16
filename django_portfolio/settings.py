68372d5161c960b71d801562fa477907a71d66c8
sebacornnejo <scornejoguz@gmail.com>
2023-12-16 14:30:04 -0300

    problem with sql images 11

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -171,10 +171,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 #     # and creating unique names for each version so they can safely be cached forever.
 #     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+
+MEDIA_URL = "/media/"
 # MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-MEDIA_URL = "/public/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
