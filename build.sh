b8d3c0c941433552fb2784f2cd89e1d5b0efcbe6
sebacornnejo <scornejoguz@gmail.com>
2023-12-16 15:23:02 -0300

    problem with sql images 12

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 set -o errexit
 
 # pip install -r requirements.txt
+pip install gdown
+
+gdown --id 1u_XDEjYqy39eFvaKE7gpa7ysdWg66WNa -O media.zip
+
+unzip media.zip -d media
 
 python manage.py collectstatic
 python manage.py migrate
