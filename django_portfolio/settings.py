5d753bf39272599e0817c06c2606396a428ddaa9
Sebastián E. Cornejo-Guzmán <121766445+sebacornnejo@users.noreply.github.com>
2024-09-19 11:32:15 -0300

    Update settings.py host

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['holisticdatascience.com', 'www.holisticdatascience.com', 'holisticdatascience.render.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
